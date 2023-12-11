9e84d8e9b649a1e8bf1b50171aa354a8285ce2e9
rohanaditya <80692896+rohanaditya@users.noreply.github.com>
2023-12-11 16:26:09 +0530

    Add files via upload

---
+++ Data/MVCDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_CRUD.Data
+{
+    public class MVCDbContext : DbContext
+    {
+        public MVCDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Models.Domain.User> Users { get; set; }
+        public DbSet<Models.Domain.Product> Products { get; set; }
+
+    }
+}
